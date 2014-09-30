24c68a371ddb38b9b4a8661446c09a2aba155895
Jorge Figueroa (Qosmio W8) <yknx4@outlook.com>
2014-09-30 08:19:54 -0500

    start coding

---
+++ src/java/main/UtilityJSFManagedBean.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package main;
+
+import javax.inject.Named;
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+ *
+ * @author Yknx
+ */
+@Named(value = "utilityJSFManagedBean")
+@ApplicationScoped
+public class UtilityJSFManagedBean {
+
+    /**
+     * Creates a new instance of UtilityJSFManagedBean
+     */
+    public UtilityJSFManagedBean() {
+    }
+    
+}
